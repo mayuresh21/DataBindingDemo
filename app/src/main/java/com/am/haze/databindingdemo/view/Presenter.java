89f6acd7ec18c978cb3ca2a7a0113cc41ca6925e
mayuresh21 <mayureshdeshmukh21@gmail.com>
2017-05-02 11:39:29 +0530

    1st commit

---
+++ app/src/main/java/com/am/haze/databindingdemo/view/Presenter.java
@@ -0,0 +1,13 @@
+package com.am.haze.databindingdemo.view;
+
+import android.view.View;
+
+/**
+ * Created by ${Mayuresh} on 4/26/2017.
+ */
+
+public class Presenter {
+    public void onSaveClick(View view){
+
+    }
+}
